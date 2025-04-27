99e8134204fc34111a342f969514790da9250727
ravi-klt <kumar.ravi.klt@gmail.com>
2025-04-27 15:10:41 +0400

    initial commit user project

---
+++ src/main/java/com/example/user_service/models/User.java
@@ -0,0 +1,45 @@
+package com.example.user_service.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank
+    private String email;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
